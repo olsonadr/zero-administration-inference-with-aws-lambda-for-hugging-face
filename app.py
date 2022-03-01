54a9c6866f71e6bc65974bd85fd3cabde66a81a0
Nick Olson <olsonnick10@yahoo.com>
2022-03-01 13:37:24 -0800

    Parseled similar incident model and config into docker container (excluding large files)

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ SPDX-License-Identifier: MIT-0
 import os
 from pathlib import Path
 from aws_cdk import (
+    CfnCondition,
+    Fn,
     aws_lambda as lambda_,
     aws_apigateway as api_gw,
     aws_efs as efs,
@@ -18,8 +20,17 @@ class ServerlessHuggingFaceStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # EFS needs to be setup in a VPC
-        vpc = ec2.Vpc(self, 'Vpc', max_azs=2)
+        # EFS needs to be setup in a VPC (but no NAT gateways, please)
+        # Source: https://github.com/aws/aws-cdk/issues/1305
+        vpc = ec2.Vpc(self, 'Vpc', max_azs=2, nat_gateways=0)
+        exclude_condition = CfnCondition(
+            self, 'exclude-default-route-subnet', expression=Fn.condition_equals(True, False))
+        for subnet in vpc.private_subnets:
+            for child in subnet.node.children:
+                if type(child) == ec2.CfnRoute:
+                    route: ec2.CfnRoute = child
+                    route.cfn_options.condition = exclude_condition  # key point here
+        # vpc = ec2.Vpc(self, 'Vpc', max_azs=2)
 
         # creates a file system in EFS to store cache models
         fs = efs.FileSystem(self, 'FileSystem',
@@ -33,7 +44,8 @@ class ServerlessHuggingFaceStack(Stack):
 
         # %%
         # iterates through the Python files in the docker directory
-        docker_folder = os.path.dirname(os.path.realpath(__file__)) + "/inference"
+        docker_folder = os.path.dirname(
+            os.path.realpath(__file__)) + "/inference"
         pathlist = Path(docker_folder).rglob('*.py')
         for path in pathlist:
             base = os.path.basename(path)
@@ -51,7 +63,13 @@ class ServerlessHuggingFaceStack(Stack):
                 filesystem=lambda_.FileSystem.from_efs_access_point(
                     access_point, '/mnt/hf_models_cache'),
                 environment={
-                    "TRANSFORMERS_CACHE": "/mnt/hf_models_cache"},
+                    "TRANSFORMERS_CACHE": "/mnt/hf_models_cache",
+                    "MODEL_DIR": "model",
+                    "MODEL_FILENAME": "pytorch_model.bin",
+                    "INCIDENTS_FILENAME": "incident_cls.pt",
+                    "CSV_FILENAME": "incidents.csv",
+                    "HF_MODEL_URI": "allenai/longformer-base-4096"
+                },
             )
 
             # adds method for the function
@@ -61,6 +79,7 @@ class ServerlessHuggingFaceStack(Stack):
                                                'method.response.header.Access-Control-Allow-Origin': "'*'"
                                            })
             ])
+
 
 app = App()
 
---
+++ inference/similar.py
@@ -0,0 +1,145 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+# Imports
+import os
+import json
+import torch
+# from pandas import read_csv
+from transformers import LongformerTokenizer, LongformerModel
+
+
+def list_files(startpath):
+    res = ""
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        res = res + ('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            res = res + ('{}{}'.format(subindent, f))
+    return res
+
+
+# # Get model and tokenizer from EFS or download it to EFS
+# model_path = os.path.join(
+#     os.environ["TRANSFORMERS_CACHE"], os.environ['MODEL_DIR'], os.environ["MODEL_FILENAME"])
+# model_dir = os.path.join(
+#     os.environ["TRANSFORMERS_CACHE"], os.environ['MODEL_DIR'])
+# hf_uri = os.environ["HF_MODEL_URI"]
+
+# # If it exists in the EFS, load from EFS
+# if os.path.isfile(model_path):
+#     tokenizer = LongformerTokenizer.from_pretrained(hf_uri)
+#     model = LongformerModel.from_pretrained(hf_uri)
+#     tokenizer.save_pretrained(model_dir)
+#     model.save_pretrained(model_dir)
+# # Else, not saved into EFS yet, get from hf and save
+# else:
+#     tokenizer = LongformerTokenizer.from_pretrained(
+#         model_dir, local_files_only=True)
+#     model = LongformerModel.from_pretrained(model_dir, local_files_only=True)
+
+# # Constants
+# incidents_path = os.path.join(
+#     os.environ["TRANSFORMERS_CACHE"], os.environ["INCIDENTS_FILENAME"])
+# csv_path = os.path.join(
+#     os.environ["TRANSFORMERS_CACHE"], os.environ["CSV_FILENAME"])
+model = LongformerModel.from_pretrained('/function/model', local_files_only=True)
+tokenizer = LongformerTokenizer.from_pretrained(
+    '/function/model', local_files_only=True)
+csv_path = '/function/model/incidents.csv'
+incidents_path = '/function/model/incident_cls.pt'
+best_of = 3
+
+# Load in a list of articles from a CSV
+tensors = torch.load(incidents_path)
+# data = read_csv(csv_path)
+
+# Test effacacy of preprocessing and meaning whole output rather than
+# stripping CLS token. (Cursory: much more effective)
+def test(text):
+    inp = tokenizer(text,
+                    padding="longest",
+                    truncation="longest_first",
+                    return_tensors="pt")
+    out = model(**inp)
+    sims = [
+        torch.nn.functional.cosine_similarity(out.last_hidden_state[0][0],
+                                              tensors[i].mean(0),
+                                              dim=-1).item()
+        if tensors[i] != None else torch.zeros(1)
+        for i in range(len(tensors))
+    ]
+    return sims
+
+def inputted(whole_text):
+    sims = [j for j in sorted(zip(test(whole_text), range(1, len(tensors) + 1)), reverse=True)]
+    best = sims[:best_of]
+    return best
+    # return sims
+
+# What to do to correctly formatted input event_text
+def process(event_text):
+    # return tokenizer(event_text)
+    return str(inputted(event_text))
+
+# Define lambda handler
+def handler(event, context):
+    # Starting point for response formatting
+    result = {
+        "isBase64Encoded": False,
+        "statusCode": 500,
+        "headers": {"Content-Type": "application/json"},
+        "multiValueHeaders": {},
+        "body": ""
+    }
+
+    # Get input from body or query string
+    if ('text' in event):
+        event_text = event['text']
+    elif ('body' in event and 'text' in event['body']):
+        event_text = event['body']['text']
+    elif ('queryStringParameters' in event and 'text' in event['queryStringParameters']):
+        event_text = event['queryStringParameters']['text']
+    else:
+        result['statusCode'] = 500
+        result['body'] = "Error! Valid input text not provided!"
+        result['headers']['Content-Type'] = "application/json"
+        return result
+
+    # Found event_text, use it and return result
+    try:
+        result['statusCode'] = 200
+        result['body'] = process(event_text)
+        # result['body'] = inputted(event_text)
+        # result['body'] = f"""
+        # isFile = {os.path.isfile('/function/model/pytorch_model.bin')};
+        # fileTree = {list_files('/function/model')};
+        # """
+        result['headers']['Content-Type'] = "application/json"
+    except:
+        result['statusCode'] = 500
+        result['headers']['Content-Type'] = "application/json"
+    return result
+
+    # # Python 3.10 required for this nicer match formatting
+    # # Get input from body or query string
+    # match event:
+    #     # If an expected format
+    #     case {'text': event_text} \
+    #             | {'body': {'text': event_text}} \
+    #             | {'queryStringParameters': {'text': event_text}}:
+    #         result['statusCode'] = 200
+    #         # result['body'] = nlp(event_text)[0]
+    #         result['body'] = inputted(event_text)
+    #         result['headers']['Content-Type'] = "application/json"
+    #         return result
+    #     # Else if input not given, return error
+    #     case _:
+    #         result['statusCode'] = 500
+    #         result['body'] = "Error! Valid input text not provided!"
+    #         result['headers']['Content-Type'] = "application/json"
+    #         return result
